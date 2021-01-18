2f3376c0b81e52c902d13d13ae4cab07aa84d5dd
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2021-01-17 22:36:50 -0600

    Added fileParser

--- src/main/java/Userinterfaces/ConsoleInterface.java
+++ src/main/java/Userinterfaces/ConsoleInterface.java
@@ -1,8 +1,19 @@
 package Userinterfaces;
 
+import java.io.IOException;
+
 public class ConsoleInterface {
 
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        try {
+//            ProcessFileParser.createFileTemplate();
+            System.out.println(ProcessFileParser.parsefile("newFile.csv").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
